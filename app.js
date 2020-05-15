d61e741766722c717bfe4ee2b91a8678341e6f65
Kevin He <kevheyk@gmail.com>
2020-05-14 20:43:30 -0600

    Complete RESTful API

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ejs = require("ejs");
 // Set Environment
 const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public/"));
 app.listen(3000, function(){
   console.log("Server started on port 3000");
@@ -28,7 +28,105 @@ const articleSchema = new mongoose.Schema({
   title:{
     type:String,
     required:true
+  },
+  content:{
+    type: String,
+    required: true
   }
 });
 
-const Ariticle = mongoose.model("Article", articleSchema);
+const Article = mongoose.model("Article", articleSchema);
+
+// Start Building API
+// GET
+
+app.route("/articles")
+.get((req, res)=>{
+  Article.find({},{},(err, docs)=>{
+    if (err){
+      res.send("Error finding all articles : " + err);
+    } else{
+      res.send(docs)
+    }
+    res.send(docs);
+  })
+})
+.post((req, res)=>{
+  const article = new Article({
+    title: req.body.title,
+    content: req.body.content 
+  });
+  article.save({validateBeforeSave:true},function(err, docs){
+    if (err){
+      res.send("Error Occurs when saving: "+ err);
+    } else{
+      res.send("New article saved")
+    }
+  });
+})
+.delete((req, res)=>{
+  Article.deleteMany({title:"Test"},(err)=>{
+    if (err){
+      res.send("Error deleting: "+err);
+    } else{
+      res.send("Success Delete");
+    }
+  });
+});
+
+
+app.get("/articles/:title",(req, res)=>{
+  Article.find({title:req.params.title},{},(err,doc)=>{
+    if(err){
+      console.log("Error finding specific article: " + err)
+    } else{
+      res.send(doc)
+    }
+  });
+});
+
+app.put("/articles/:title", (req, res)=>{
+  console.log(req.body)
+  Article.update(
+    {title:req.params.title},
+    { //$set does nothing different.
+      title:req.body.title,
+      content: req.body.content}, 
+    {overwrite:true}, 
+    //Overwrite: Defualt false
+    // True: removed the original document.
+    (err)=>{
+      if (err){
+        console.log("Error replacing:" + err);
+      } else{
+        res.send("Success replace article")
+      }
+    })
+})
+
+
+app.patch("/articles/:title", (req, res)=>{
+  Article.update(
+    {title:req.params.title},
+    req.body,//$set makes no difference
+    (err)=>{
+      if (err){
+        console.log("Error updating:" + err);
+      } else{
+        res.send("Success updated article")
+      }
+    })
+})
+
+app.delete("/articles/:title", (req, res)=>{
+  Article.deleteOne(
+    {title:req.params.title},
+    (err)=>{
+      if (err){
+        console.log("Error deleting:" + err);
+      } else{
+        res.send("Success deleted article")
+      }
+    })
+})
+
